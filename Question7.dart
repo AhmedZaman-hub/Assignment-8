e2ae62294e59ae61347c05d78afbeb7f79cbf920
Ahmed Zaman <wannad580@gmail.com>
2024-05-04 22:58:35 +0500

    q7 done

---
+++ Question7.dart
@@ -0,0 +1,17 @@
+void main() {
+  int count = 0;
+
+  for (int number = 2; count < 10; number++) {
+    bool isPrime = true;
+    for (int i = 2; i <= number ~/ 2; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      print(number);
+      count++;
+    }
+  }
+}
